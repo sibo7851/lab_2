47ab93a13d95d28eadc36e0fff4a51876562350d
sibo7851 <sibo7851@colorado.edu>
2019-01-30 13:26:07 -0500

    recitation commit

--- lab2_scripts.sh
+++ lab2_scripts.sh
@@ -9,4 +9,11 @@ read in2
 echo "Enter regular expression"
 read in1
 echo "Command Line Argument1: $in1 $in2"
-grep $in1 $in2
+grep $in1 $in2 > command_results.txt
+grep -P -c "^\d{3}-\d{3}-\d{4}" regex_practice.txt
+grep -c "@" regex_practice.txt
+grep -P "^303-\d{3}-\d{4}" regex_practice.txt > phone_results.txt
+grep -P "^((?!@geocities.com).)*$" regex_practice.txt > email_results.txt
+#^((?!hede).)*$
+#grep "@geocities.com" regex_practice.txt
+
